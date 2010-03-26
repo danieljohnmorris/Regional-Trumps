3d87597492d57eeff7f05979c9c42aff7f91a434
Daniel Morris <daniel.john.morris@googlemail.com>
2010-03-26 20:53:11 +0000

    remove region files

--- app/controllers/admin/regions_controller.rb
+++
@@ -1,2 +0,0 @@
-class Admin::RegionsController < ApplicationController
-end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ActiveRecord::Base
+  belongs_to :parent, :class_name => "Card", :foreign_key => "parent_id"
 end
